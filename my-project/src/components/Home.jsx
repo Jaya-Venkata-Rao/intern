bffe1d45f1ee0d9d406c90dcda46b2da0f02218d
Jaya-Venkata_Rao <22pa1a12h6@vishnu.in>
2025-01-06 21:18:39 +0530

    Changes in CSS

--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -5,7 +5,13 @@ const Home = () => {
   return (
     <div className="w-full min-h-[120vh] bg-cover bg-center bg-[url('assets/ayodhya.webp')] relative">
       <Navbar />
-  
+      <div className="px-4 py-10">
+        <h3 className='text-center text-xl py-10 text-white/75'>Simple - Unique - Friendly</h3>
+        <h1 className='text-center text-white text-6xl font-bold'>
+          Make Yourself At Home <br /> In Our{' '}
+          <span className="text-pink-600">Guest House</span>.
+        </h1>
+      </div>
     </div>
   );
 };
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -10,7 +10,20 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-full max-w-[1240px] mx-auto px-4 py-4 relative">
-      <h1 className="w-full text-xl font-bold text-[white]">
+      {/* Static Pink Background for Mobile */}
+      <div className="block md:hidden fixed top-0 left-0 w-full h-16 bg-[#e82d8d] z-40">
+        <div className="flex justify-between items-center h-full max-w-[1240px] mx-auto px-4">
+          <h1 className="text-xl font-bold text-white">
+            Kingsukh <br /> Guest House
+          </h1>
+          <div onClick={handleNav} className="cursor-pointer z-50">
+            {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+          </div>
+        </div>
+      </div>
+
+      {/* Desktop Navbar */}
+      <h1 className="hidden md:block w-full text-xl font-bold text-white">
         Kingsukh <br /> Guest House
       </h1>
       <ul className="hidden md:flex text-white">
@@ -22,22 +35,15 @@ const Navbar = () => {
         <li className="p-4">Contact</li>
       </ul>
 
-      {/* Menu Icon */}
-      <div onClick={handleNav} className="block md:hidden cursor-pointer z-50">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-
       {/* Mobile Menu */}
       <div
         className={`${
           nav
-            ? 'fixed top-0 left-0 w-full h-screen bg-[#d40080] md:bg-[#f01880] ease-in-out duration-500'
+            ? 'fixed top-0 left-0 w-full h-[75%] bg-[#d40080]/85 text-center ease-in-out duration-500'
             : 'fixed top-[-100%]'
-        }`}
+        } md:hidden z-30`}
       >
-        <h1 className="w-full p-4 text-xl font-bold text-[white] bg-[#d40080] md:bg-[#f01882]">
-          Kingsukh <br /> Guest House
-        </h1>
+        
         <ul className="uppercase p-4">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">About</li>
@@ -47,7 +53,6 @@ const Navbar = () => {
           <li className="p-4 border-b border-gray-600">Contact</li>
         </ul>
       </div>
-      
     </div>
   );
 };
